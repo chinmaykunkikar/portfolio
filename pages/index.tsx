aedc3273b65488db39d71898c8d921bf11fa90e2
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2022-12-15 02:24:48 +0530

    pages: Remove unused code

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,3 @@
-import Head from "next/head";
-
 export default function Home() {
-  return (
-    <div className="">
-      <Head>
-        <title>Chinmay Kunkikar</title>
-        <meta name="description" content="Portfolio of Chinmay Kunkikar" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className=""></main>
-
-      <footer className=""></footer>
-    </div>
-  );
+  return <div className="min-h-screen"></div>;
 }
