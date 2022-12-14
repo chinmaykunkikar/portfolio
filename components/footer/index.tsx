855fb58046449c70a679ccf685ffc01995ed6dce
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2022-12-15 02:24:00 +0530

    compnents: Add <Footer />

---
+++ components/footer/index.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <div className="flex justify-center px-16 py-4">
+      <span className="text-gray-400">
+        Built by <span className="font-bold">Chinmay</span>
+      </span>
+    </div>
+  );
+}
