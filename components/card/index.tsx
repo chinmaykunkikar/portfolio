06208a4c716ed4f78306e47ab11a14ef2fb30c3b
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2022-12-15 21:20:24 +0530

    components: Add <Card /> component

---
+++ components/card/index.tsx
@@ -0,0 +1,7 @@
+export default function Card() {
+  return (
+    <div className="group space-y-3 rounded-lg bg-white p-6 hover:drop-shadow-xl">
+      Hello
+    </div>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,17 @@
+import Card from "../components/card";
+
 export default function Home() {
-  return <div className="min-h-screen"></div>;
+  return (
+    <div className="grid-rows-auto mx-8 grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  );
 }
