b6a74aba8a40b81253770f0a7f41c54d2e0cc7f4
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2022-12-15 18:39:09 +0530

    layout: Replace white with a base color

--- components/layout.tsx
+++ components/layout.tsx
@@ -3,7 +3,7 @@ import Navbar from "./navbar";
 
 export default function Layout({ children }: any) {
   return (
-    <div className="container">
+    <div className="container bg-neutral-100">
       <Navbar />
       <main>{children}</main>
       <Footer />
