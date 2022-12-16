1057b80d5557c7363b49b9210bd6d7e3bcf6ac36
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2022-12-16 19:58:00 +0530

    Home: Remove Resume in favour of Twitter card

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,8 +5,8 @@ export default function Home() {
     <div className="grid-rows-auto grid min-h-screen grid-cols-1 gap-6 px-16 pb-8 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
       <Card className="col-span-2">Hi, I&apos;m Chinmay</Card>
       <Card>GitHub</Card>
-      <Card>Resume</Card>
       <Card>LinkedIn</Card>
+      <Card>Twitter</Card>
       <Card>Projects</Card>
       <Card className="col-span-2">Skills</Card>
       <Card>Spotify</Card>
