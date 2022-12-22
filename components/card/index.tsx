d99cc5f3080e3fa956584340ecc3a7ab19f3d198
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2022-12-23 01:43:36 +0530

    Card: Allow multiple children

--- components/card/index.tsx
+++ components/card/index.tsx
@@ -2,14 +2,14 @@ import React from "react";
 
 export type CardPropsType = {
   className?: String;
-  children?: JSX.Element | String;
+  children?: JSX.Element | JSX.Element[] | String;
   colSpan?: number;
 };
 
 export default function Card({ children, className }: CardPropsType) {
   return (
     <div
-      className={`min-h-[20vh] rounded-2xl bg-white p-6 text-xl font-semibold text-neutral-600 transition-all duration-300 ease-in hover:drop-shadow-xl ${className}`}
+      className={`font-semibold min-h-[20vh] rounded-2xl bg-white p-6 text-xl text-neutral-600 transition-all duration-300 ease-in hover:drop-shadow-xl ${className}`}
     >
       {children}
     </div>
