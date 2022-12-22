7708ef799ef6ba54e09528b87c7a0fcdb47fbafc
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2022-12-23 03:20:18 +0530

    widget: Add GitHubWidget

--- components/widgets/github/index.tsx
+++ components/widgets/github/index.tsx
@@ -1,3 +1,24 @@
+import Image from "next/image";
+import Card from "../../card";
+import { ArrowRightCircleIcon } from "@heroicons/react/24/outline";
+
 export default function GitHubWidget() {
-  return <div></div>;
+  return (
+    <Card className="flex flex-col items-center justify-center">
+      <Image
+        src="/github.svg"
+        width={128}
+        height={128}
+        alt="Chinmay's GitHub profile"
+      />
+      <a
+        href="https://github.com/chinmaykunkikar/"
+        target="_blank"
+        className="text-bold mt-4 mr-4 self-end hover:text-neutral-500"
+        rel="noreferrer"
+      >
+        <ArrowRightCircleIcon width={42} height={42} />
+      </a>
+    </Card>
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,12 @@
 import Card from "../components/card";
+import GitHubWidget from "../components/widgets/github";
 import HelloWidget from "../components/widgets/hello";
 
 export default function Home() {
   return (
     <div className="grid-rows-auto grid min-h-screen grid-cols-1 gap-6 px-16 pb-8 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
       <HelloWidget />
-      <Card>GitHub</Card>
+      <GitHubWidget />
       <Card>LinkedIn</Card>
       <Card>Twitter</Card>
       <Card>Projects</Card>
