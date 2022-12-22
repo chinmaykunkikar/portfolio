1c5901accd02bb325ff217f514ae3ff753ad5f3b
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2022-12-23 02:24:17 +0530

    Card: Handle rest props

--- components/card/index.tsx
+++ components/card/index.tsx
@@ -3,13 +3,14 @@ import React from "react";
 export type CardPropsType = {
   className?: String;
   children?: JSX.Element | JSX.Element[] | String;
-  colSpan?: number;
+  [props: string]: any;
 };
 
-export default function Card({ children, className }: CardPropsType) {
+export default function Card({ children, className, ...props }: CardPropsType) {
   return (
     <div
       className={`font-semibold min-h-[20vh] rounded-2xl bg-white p-6 text-xl text-neutral-600 transition-all duration-300 ease-in hover:drop-shadow-xl ${className}`}
+      {...props}
     >
       {children}
     </div>
