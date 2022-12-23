cf36ca004464cbc0d130692656a49ea7b0f456fe
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2022-12-23 21:34:47 +0530

    SocialWidget: Make the arrow position absolute so that logos are at dead center

--- components/widgets/social/index.tsx
+++ components/widgets/social/index.tsx
@@ -18,21 +18,25 @@ export default function SocialWidget({
   socialUrl,
 }: SocialPropTypes) {
   return (
-    <Card className="flex flex-col items-center justify-center">
-      <Image
-        src={imgSrc}
-        width={imgWidth ?? 128}
-        height={imgHeight ?? 128}
-        alt={imgAlt}
-      />
-      <a
-        href={socialUrl}
-        target="_blank"
-        className="text-bold mt-4 mr-4 self-end hover:text-neutral-500"
-        rel="noreferrer"
-      >
-        <ArrowRightCircleIcon width={42} height={42} />
-      </a>
+    <Card className="group relative">
+      <div className="flex h-full w-full items-center justify-center">
+        <Image
+          src={imgSrc}
+          width={imgWidth ?? 128}
+          height={imgHeight ?? 128}
+          alt={imgAlt}
+        />
+        <div className="absolute right-4 bottom-4 flex -rotate-45 items-center justify-center group-hover:text-neutral-500">
+          <a
+            href={socialUrl}
+            target="_blank"
+            className="text-bold rounded-full hover:bg-neutral-300"
+            rel="noreferrer"
+          >
+            <ArrowRightCircleIcon width={42} height={42} />
+          </a>
+        </div>
+      </div>
     </Card>
   );
 }
