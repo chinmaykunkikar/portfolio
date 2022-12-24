080447c88f9e8b33f6ce1be22f86a3f6fb4d9e6c
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2022-12-25 02:01:10 +0530

    Footer: Add some flames :fire:

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -1,8 +1,20 @@
+import { Player } from "@lottiefiles/react-lottie-player";
+import Fire from "../../public/fire.json";
+
 export default function Footer() {
   return (
     <div className="flex justify-center px-16 py-4">
-      <span className="text-neutral-500">
-        Built by <span className="font-bold">Chinmay</span>
+      <span className="flex items-end text-neutral-500">
+        <span>Built with</span>
+        <Player
+          src={Fire}
+          autoplay={true}
+          loop={true}
+          style={{ width: "2em", height: "2em" }}
+        />{" "}
+        <span>
+          by <span className="font-bold">Chinmay</span>
+        </span>
       </span>
     </div>
   );
