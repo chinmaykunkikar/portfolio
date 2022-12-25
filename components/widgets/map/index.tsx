13e8fe1d75621aa4de213beea22072403fef8224
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2022-12-25 23:58:58 +0530

    MapWidget: Introduce MapWidget This will be disabled until implemented nicely

---
+++ components/widgets/map/index.tsx
@@ -0,0 +1,30 @@
+import Map, { Marker } from "react-map-gl";
+import Card from "../../card";
+export default function MapWidget() {
+  return (
+    <Card className="p-0">
+      <Map
+        initialViewState={{
+          latitude: process.env.MAP_LATITUDE,
+          longitude: process.env.MAP_LONGITUDE,
+          zoom: 12,
+        }}
+        minZoom={12}
+        maxZoom={12}
+        style={{
+          width: "100%",
+          height: "100%",
+        }}
+        mapStyle="mapbox://styles/chinmaykunkikar/clc3j5l73005k14mpx2ghpj5x"
+        mapboxAccessToken={process.env.MAPBOX_TOKEN}
+      >
+        <Marker
+          latitude={process.env.MAP_LATITUDE}
+          longitude={process.env.MAP_LONGITUDE}
+          anchor="center"
+          color="#DD2222"
+        />
+      </Map>
+    </Card>
+  );
+}
--- types/environment.d.ts
+++ types/environment.d.ts
@@ -6,6 +6,9 @@ declare global {
       SPOTIFY_CLIENT_ID: string;
       SPOTIFY_CLIENT_SECRET: string;
       SPOTIFY_REFRESH_TOKEN: string;
+      MAPBOX_ACCESS_TOKEN: string;
+      MAP_LATITUDE: number;
+      MAP_LONGITUDE: number;
     }
   }
 }
