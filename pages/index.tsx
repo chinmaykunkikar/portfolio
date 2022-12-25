131d8ca481e302eae49ce37080b4bb3d93d39cfe
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2022-12-26 00:01:10 +0530

    index: disable MapWidget for now Need to figure out how it works

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,6 @@ import Card from "../components/card";
 import GitHubWidget from "../components/widgets/github";
 import HelloWidget from "../components/widgets/hello";
 import LinkedInWidget from "../components/widgets/linkedin";
-import MapWidget from "../components/widgets/map";
 import SpotifyWidget from "../components/widgets/spotify";
 import TwitterWidget from "../components/widgets/twitter";
 
@@ -10,7 +9,7 @@ export default function Home() {
   return (
     <div className="grid-rows-auto grid min-h-screen grid-cols-1 gap-6 px-32 pb-8 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
       <HelloWidget />
-      <MapWidget />
+      <Card>Location Widget</Card>
       <SpotifyWidget />
       <TwitterWidget />
       <LinkedInWidget />
