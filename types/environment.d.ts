d52be4e4131b3b13e6c6ac520a425d22c01d3332
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2022-12-25 23:57:38 +0530

    types: Introduce environment.d.ts

---
+++ types/environment.d.ts
@@ -0,0 +1,11 @@
+export {};
+
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      SPOTIFY_CLIENT_ID: string;
+      SPOTIFY_CLIENT_SECRET: string;
+      SPOTIFY_REFRESH_TOKEN: string;
+    }
+  }
+}
