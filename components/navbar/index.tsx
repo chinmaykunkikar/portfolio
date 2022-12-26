76ad2d87ebb6c6a0b5dff156e675284559e005f5
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2022-12-26 22:13:53 +0530

    Navbar: Add active color to the links

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import useSWR from "swr";
 import fetcher from "../../lib/fetcher";
 import Button from "../button";
@@ -8,13 +9,39 @@ type CommitSha = {
   sha: string;
 };
 
+type LinkWrapperProps = {
+  children: string | JSX.Element | JSX.Element[];
+  linkHref: string;
+  otherClasses?: string;
+  [props: string]: any;
+};
+
 export default function Navbar() {
+  const { pathname } = useRouter();
+
   const { data } = useSWR<CommitSha>(
     "https://api.github.com/repos/chinmaykunkikar/portfolio/commits/next",
     fetcher
   );
-
   const shortSha = data?.sha.slice(0, 7);
+
+  function LinkWrapper({
+    children,
+    linkHref,
+    otherClasses,
+    ...props
+  }: LinkWrapperProps) {
+    return (
+      <Link href={linkHref} className={otherClasses}>
+        <span
+          className={pathname == linkHref ? "text-neutral-500" : ""}
+          {...props}
+        >
+          {children}
+        </span>
+      </Link>
+    );
+  }
 
   return (
     <>
@@ -23,7 +50,7 @@ export default function Navbar() {
         <meta name="description" content="Portfolio of Chinmay Kunkikar" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="flex w-screen flex-col items-center justify-between gap-4 px-32 py-8 text-neutral-600 lg:flex-row lg:gap-0">
+      <div className="flex w-screen flex-col items-center justify-between gap-4 px-32 py-8 text-neutral-700 lg:flex-row lg:gap-0">
         <span className="text-xl">
           <h3 className="cursor-default	select-none	font-mono">
             [chinmay@web{" "}
@@ -35,25 +62,21 @@ export default function Navbar() {
             >
               {shortSha}
             </a>
-            ]$&#8202;<span className="animate-pulse">&#9601;</span>
+            ]$&#8202;
+            <span className="animate-pulse">&#9601;</span>
           </h3>
         </span>
         <div className="flex h-fit items-center gap-8">
-          <Link href="/">
-            <Button>Home</Button>
-          </Link>
-          <Link href="/works">
-            <Button>Works</Button>
-          </Link>
-          <Link href="/about">
+          <LinkWrapper linkHref="/">Home</LinkWrapper>
+          <LinkWrapper linkHref="/about">
             <Button>About</Button>
-          </Link>
-          <Link href="/resume">
-            <Button>Resume</Button>
-          </Link>
-          <Link href="/contact">
+          </LinkWrapper>
+          <LinkWrapper linkHref="/works">
+            <Button>Works</Button>
+          </LinkWrapper>
+          <LinkWrapper linkHref="/contact">
             <Button>Contact</Button>
-          </Link>
+          </LinkWrapper>
         </div>
       </div>
     </>
