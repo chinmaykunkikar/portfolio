6c502f78fc2eeaf3bb3c40c95f8b647f0c2fe9d0
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2022-12-30 01:36:43 +0530

    layout: Fix the excess x-overflow [Fixes#4] Apparantly container actually makes the layout more rigid.

--- components/layout.tsx
+++ components/layout.tsx
@@ -3,7 +3,7 @@ import Navbar from "@components/navbar";
 
 export default function Layout({ children }: any) {
   return (
-    <div className="container bg-neutral-100">
+    <div className="overflow-x-hidden bg-neutral-100">
       <Navbar />
       <main>{children}</main>
       <Footer />
