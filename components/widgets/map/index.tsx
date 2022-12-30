04a84bb35cde3e7e123b12c0e945272b3258e02c
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2022-12-30 19:01:57 +0530

    MapWidget: Fix overflow issue

--- components/widgets/map/index.tsx
+++ components/widgets/map/index.tsx
@@ -1,30 +1,26 @@
 import Card from "@components/card";
-import Map, { Marker } from "react-map-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
+import Map from "react-map-gl";
+
 export default function MapWidget() {
   return (
-    <Card className="p-0">
+    <Card className="p-0 overflow-clip">
       <Map
+        reuseMaps
         initialViewState={{
           latitude: process.env.MAP_LATITUDE,
           longitude: process.env.MAP_LONGITUDE,
-          zoom: 12,
+          zoom: 13,
         }}
-        minZoom={12}
-        maxZoom={12}
+        minZoom={13}
+        maxZoom={13}
         style={{
           width: "100%",
           height: "100%",
         }}
         mapStyle="mapbox://styles/chinmaykunkikar/clc3j5l73005k14mpx2ghpj5x"
         mapboxAccessToken={process.env.MAPBOX_TOKEN}
-      >
-        <Marker
-          latitude={process.env.MAP_LATITUDE}
-          longitude={process.env.MAP_LONGITUDE}
-          anchor="center"
-          color="#DD2222"
-        />
-      </Map>
+      />
     </Card>
   );
 }
