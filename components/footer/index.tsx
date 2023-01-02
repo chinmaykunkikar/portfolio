61377b0dc395aa3cc6c6f951762cab6041964f03
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2023-01-03 01:56:44 +0530

    Footer: 🇮🇳 💗

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -1,19 +1,16 @@
 import { Player } from "@lottiefiles/react-lottie-player";
-import Fire from "@public/fire.json";
+import Heart from "@public/heart.json";
 
 export default function Footer() {
   return (
     <div className="flex justify-center px-16 py-4">
       <span className="flex items-end text-neutral-500">
-        <span>Built with</span>
-        <Player
-          src={Fire}
-          autoplay={true}
-          loop={true}
-          style={{ width: "2em", height: "2em" }}
-        />{" "}
         <span>
-          by <span className="font-bold">Chinmay</span>
+          Built in <span className="text-xl">🇮🇳</span> with&nbsp;
+        </span>
+        <Player src={Heart} autoplay={true} loop={true} className="w-8 h-8" />
+        <span>
+          &nbsp;by <span className="font-bold">Chinmay Kunkikar</span>
         </span>
       </span>
     </div>
