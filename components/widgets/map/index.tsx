831c6295bb58a49fe4d54b6bf28a845f294e7a89
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2023-01-04 01:46:09 +0530

    MapWidget: Initial work for zoom out button

--- components/widgets/map/index.tsx
+++ components/widgets/map/index.tsx
@@ -1,15 +1,40 @@
 import Card from "@components/card";
-import { ClockIcon } from "@heroicons/react/24/outline";
+import { ClockIcon, MinusIcon } from "@heroicons/react/24/outline";
 import "mapbox-gl/dist/mapbox-gl.css";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import Map, { Marker } from "react-map-gl";
 
 export default function MapWidget() {
   const longitude = process.env.NEXT_PUBLIC_MAP_LONGITUDE;
   const latitude = process.env.NEXT_PUBLIC_MAP_LATITUDE;
-  const zoom = 13;
+  const defaultZoom = 13;
+  const accentColor = "#ac92fa";
+  const initialViewState = {
+    longitude,
+    latitude,
+    zoom: defaultZoom,
+  };
 
   const [date, setDate] = useState(new Date());
+  const [settings, setSettings] = useState({
+    keyboard: false,
+    touchPitch: false,
+    minZoom: 6,
+    maxZoom: 13,
+    pitch: 40,
+    refreshExpiredTiles: false,
+    reuseMaps: true,
+  });
+
+  const updateSettings = useCallback(
+    // TODO Debug this
+    (name: any, value: any) =>
+      setSettings((s) => ({
+        ...s,
+        zoom: defaultZoom - 3,
+      })),
+    []
+  );
 
   const dateOptions: Intl.DateTimeFormatOptions = {
     timeZone: "Asia/Kolkata",
@@ -34,7 +59,12 @@ export default function MapWidget() {
     <Card className="select-none overflow-clip p-0 [&_canvas]:outline-0">
       <div className="absolute right-4 left-4 bottom-4 z-20 flex items-center justify-center">
         <div className="flex select-none items-center justify-between rounded-full bg-neutral-50 px-3 py-1 shadow-lg">
-          <ClockIcon width={20} height={20} stroke="#ac92fa" strokeWidth={2} />
+          <ClockIcon
+            width={20}
+            height={20}
+            stroke={accentColor}
+            strokeWidth={2}
+          />
           <div className="ml-2 font-bold">
             {date.toLocaleTimeString("en-IN", dateOptions)}
           </div>
@@ -49,27 +79,28 @@ export default function MapWidget() {
         style={{ visibility: "hidden" }}
         mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN}
         mapStyle="mapbox://styles/chinmaykunkikar/clc3j5l73005k14mpx2ghpj5x"
-        initialViewState={{
-          longitude,
-          latitude,
-          zoom,
-        }}
+        initialViewState={initialViewState}
+        {...settings}
         onLoad={() => {
           const dynamicMap = document.getElementById("dynamic_map");
           const staticMap = document.getElementById("static_map");
           if (dynamicMap !== null) dynamicMap.style.visibility = "visible";
           if (staticMap !== null) staticMap.style.visibility = "hidden";
         }}
-        minZoom={6}
-        maxZoom={13}
-        pitch={40}
-        dragRotate={false}
-        keyboard={false}
-        touchPitch={false}
-        refreshExpiredTiles={false}
-        reuseMaps
       >
-        <Marker longitude={longitude} latitude={latitude} color="#ac92fa" />
+        <Marker longitude={longitude} latitude={latitude} color={accentColor} />
+        <button
+          className="absolute left-4 bottom-4 hidden items-center justify-center rounded-full
+        bg-neutral-50 transition-all hover:ring-8 hover:ring-[#ac92fa]/[0.5]"
+        >
+          <MinusIcon
+            className="m-1 p-1"
+            strokeWidth={3}
+            width={24}
+            height={24}
+            color={accentColor}
+          />
+        </button>
       </Map>
     </Card>
   );
