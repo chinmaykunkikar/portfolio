10e99e5fc933f0115660ebe35e72ba7496b69369
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2023-01-03 15:14:05 +0530

    widgets: Build ExperienceWidget

--- components/widgets/experience/index.tsx
+++ components/widgets/experience/index.tsx
@@ -1,5 +1,23 @@
 import Card from "@components/card";
 
 export default function ExperienceWidget() {
-  return <Card className="md:col-span-2">Experience</Card>;
+  return (
+    <Card className="md:col-span-2">
+      <div className="flex flex-col justify-center w-full-h-full px-4 mt-16">
+        <div className="text-3xl font-bold select-none">Experience</div>
+        <div className="flex flex-col font-mono mt-8 text-neutral-500 cursor-default">
+          <div className="flex justify-between my-2 text-xl items-center">
+            <div className="flex-1">MetaBrainz Foundation</div>
+            <div className="text-sm mr-2">Student Intern</div>
+            <div className="text-sm font-bold">2022</div>
+          </div>
+          <div className="flex justify-between my-2 text-xl items-center">
+            <div className="flex-1">Steinn Labs</div>
+            <div className="text-sm mr-2">React Intern</div>
+            <div className="text-sm font-bold">2021</div>
+          </div>
+        </div>
+      </div>
+    </Card>
+  );
 }
