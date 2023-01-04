0142ce9b7a23809664a5674a1d79a4803bd189b9
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2023-01-04 13:34:22 +0530

    Footer: add responsiveness

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -3,12 +3,15 @@ import Heart from "@public/emoji/heart.json";
 
 export default function Footer() {
   return (
-    <div className="flex justify-center p-4 lg:px-16 lg:py-4">
+    <div className="flex justify-center p-4 text-xs lg:px-16 lg:py-4 lg:text-base">
       <span className="flex items-end text-neutral-500">
-        <span>
-          Built in <span className="text-xl">🇮🇳</span> with&nbsp;
-        </span>
-        <Player src={Heart} autoplay={true} loop={true} className="h-8 w-8" />
+        <span>Built with</span>
+        <Player
+          src={Heart}
+          autoplay={true}
+          loop={true}
+          className="h-6 w-6 lg:h-8 lg:w-8"
+        />
         <span>
           &nbsp;by <span className="font-bold">Chinmay Kunkikar</span>
         </span>
