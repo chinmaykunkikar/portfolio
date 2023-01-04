ce1e2cba5b4e13a0899be15d5daa2c7b9d7610f1
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2023-01-04 13:36:37 +0530

    MapWidget: Separate out TimeWidget MapWidget was getting lengthier

--- components/widgets/map/index.tsx
+++ components/widgets/map/index.tsx
@@ -1,7 +1,8 @@
 import Card from "@components/card";
-import { ClockIcon, MinusIcon } from "@heroicons/react/24/outline";
+import { MinusIcon } from "@heroicons/react/24/outline";
+import TimeWidget from "@widgets/time";
 import "mapbox-gl/dist/mapbox-gl.css";
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useState } from "react";
 import Map, { Marker } from "react-map-gl";
 
 export default function MapWidget() {
@@ -15,7 +16,6 @@ export default function MapWidget() {
     zoom: defaultZoom,
   };
 
-  const [date, setDate] = useState(new Date());
   const [settings, setSettings] = useState({
     keyboard: false,
     touchPitch: false,
@@ -36,40 +36,9 @@ export default function MapWidget() {
     []
   );
 
-  const dateOptions: Intl.DateTimeFormatOptions = {
-    timeZone: "Asia/Kolkata",
-    hour: "2-digit",
-    minute: "2-digit",
-    hour12: true,
-    hourCycle: "h12",
-  };
-
-  function refreshClock() {
-    setDate(new Date());
-  }
-
-  useEffect(() => {
-    const timerId = setInterval(refreshClock, 1000);
-    return function cleanup() {
-      clearInterval(timerId);
-    };
-  }, []);
-
   return (
     <Card className="select-none overflow-clip p-0 [&_canvas]:outline-0">
-      <div className="absolute right-4 left-4 bottom-4 z-20 flex items-center justify-center">
-        <div className="flex select-none items-center justify-between rounded-full bg-neutral-50 px-3 py-1 shadow-lg">
-          <ClockIcon
-            width={20}
-            height={20}
-            stroke={accentColor}
-            strokeWidth={2}
-          />
-          <div className="ml-2 font-bold">
-            {date.toLocaleTimeString("en-IN", dateOptions)}
-          </div>
-        </div>
-      </div>
+      <TimeWidget />
       <div
         id="static_map"
         className="absolute top-0 left-0 z-10 h-full w-full bg-static-map bg-contain bg-center"
---
+++ components/widgets/time/index.tsx
@@ -0,0 +1,36 @@
+import { ClockIcon } from "@heroicons/react/24/outline";
+import { useEffect, useState } from "react";
+
+export default function TimeWidget() {
+  const [date, setDate] = useState(new Date());
+
+  const dateOptions: Intl.DateTimeFormatOptions = {
+    timeZone: "Asia/Kolkata",
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: true,
+    hourCycle: "h12",
+  };
+
+  function refreshClock() {
+    setDate(new Date());
+  }
+
+  useEffect(() => {
+    const timerId = setInterval(refreshClock, 1000);
+    return function cleanup() {
+      clearInterval(timerId);
+    };
+  }, []);
+
+  return (
+    <div className="absolute right-4 left-4 bottom-4 z-20 flex items-center justify-center">
+      <div className="flex select-none items-center justify-between rounded-full bg-neutral-50 px-3 py-1 shadow-lg">
+        <ClockIcon width={20} height={20} stroke="#ac92fa" strokeWidth={2} />
+        <div className="ml-2 font-bold">
+          {date.toLocaleTimeString("en-IN", dateOptions)}
+        </div>
+      </div>
+    </div>
+  );
+}
