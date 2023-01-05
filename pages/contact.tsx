2b1e2bada88555876b47e2630b459688193bb84d
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2023-01-05 20:04:22 +0530

    contact: Build a basic contact form Try yellow accent as well.

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,9 +1,69 @@
+import Card from "@components/card";
+
 export default function Contact() {
   return (
-    <div className="flex min-h-screen justify-center">
-      <h1 className="flex justify-center px-10 py-8 text-4xl font-bold text-neutral-200 lg:text-9xl">
-        Contact Me
-      </h1>
+    <div className="flex min-h-screen items-center">
+      <Card className="mx-auto max-h-[32rem] w-full hover:drop-shadow-none">
+        <p className="mb-2 p-2 font-bold text-neutral-500">
+          Write your message here
+        </p>
+        <div className="p-2">
+          <div className="relative">
+            <label
+              htmlFor="name"
+              className="text-sm leading-7 text-neutral-600"
+            >
+              Name
+            </label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              className="w-full rounded border border-neutral-300 bg-neutral-100 py-1 px-3 text-base leading-8 text-neutral-700 outline-none transition-colors duration-200 ease-in-out focus:border-yellow-500"
+            />
+          </div>
+        </div>
+        <div className="p-2">
+          <div className="relative">
+            <label
+              htmlFor="email"
+              className="text-sm leading-7 text-neutral-600"
+            >
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              className="w-full rounded border border-neutral-300 bg-neutral-100 py-1 px-3 text-base leading-8 text-neutral-700 outline-none transition-colors duration-200 ease-in-out focus:border-yellow-500"
+            />
+          </div>
+        </div>
+        <div className="p-2">
+          <div className="relative">
+            <label
+              htmlFor="message"
+              className="text-sm leading-7 text-neutral-600"
+            >
+              Message
+            </label>
+            <textarea
+              id="message"
+              name="message"
+              className="h-32 w-full resize-none rounded border border-neutral-300 bg-neutral-100 py-1 px-3 text-base leading-6 text-neutral-700 outline-none transition-colors duration-200 ease-in-out focus:border-yellow-500"
+            ></textarea>
+          </div>
+        </div>
+        <div className="w-full p-2">
+          <button className="mx-auto flex rounded-2xl border-0 bg-yellow-500 py-2 px-8 text-lg text-white focus:outline-none hover:bg-yellow-600">
+            Send
+          </button>
+        </div>
+      </Card>
+
+      <div className="ml-8 text-3xl font-bold text-neutral-400">
+        Let&apos;s work on a project together!
+      </div>
     </div>
   );
 }
