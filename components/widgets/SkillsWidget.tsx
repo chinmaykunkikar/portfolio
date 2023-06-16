85e325a4087b688d35d1ad4838298f318d5d4689
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2023-06-16 18:41:34 +0530

    SkillsWidget: Adjust all spacings of the SkillsWidget

--- components/widgets/SkillsWidget.tsx
+++ components/widgets/SkillsWidget.tsx
@@ -15,18 +15,20 @@ import WebpackLogo from "@components/drawables/webpack";
 export default function SkillsWidget() {
   function SkillWrapper({ icon: SkillIcon, label }: any) {
     return (
-      <div className="m-4 flex flex-col items-center justify-start">
+      <div className="m-2 flex flex-col items-center justify-start">
         <SkillIcon width={64} height={64} />
-        <span className="mt-2 select-none text-xs font-bold">{label}</span>
+        <span className="mt-1 select-none text-xs font-bold">{label}</span>
       </div>
     );
   }
 
   return (
     <Card className="flex max-h-80 flex-col md:col-span-2">
-      <div className="mx-auto mb-4 select-none text-2xl font-bold">Skills</div>
+      <div className="mx-auto mb-2 select-none text-2xl font-bold">
+        My toolkit
+      </div>
       <div className="overflow-y-auto scroll-smooth scrollbar-thin scrollbar-track-neutral-200 scrollbar-thumb-neutral-500 scrollbar-track-rounded-full scrollbar-thumb-rounded-full">
-        <div className="grid-rows-auto grid grid-cols-2 gap-4 p-4 md:grid-cols-3 lg:grid-cols-4">
+        <div className="grid-rows-auto grid grid-cols-2 gap-3 p-2 md:grid-cols-3 lg:grid-cols-4">
           <SkillWrapper icon={JavaScriptLogo} label="JavaScript ES6" />
           <SkillWrapper icon={ReactLogo} label="ReactJS" />
           <SkillWrapper icon={TypeScriptLogo} label="TypeScript" />
