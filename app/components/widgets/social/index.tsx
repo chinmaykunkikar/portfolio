019f6c76cae8881eaf09c5e1f5b43e578f395789
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2023-08-29 01:45:48 +0530

    SocialWidget: Remove a level of  nesting

--- app/components/widgets/social/index.tsx
+++ app/components/widgets/social/index.tsx
@@ -1,5 +1,6 @@
 import Card from "@components/Card";
 import CardLinkButton from "@components/CardLinkButton";
+import { twMerge } from "tailwind-merge";
 
 export type SocialPropTypes = {
   widgetBgColor?: string;
@@ -17,18 +18,21 @@ export default function SocialWidget({
   socialUrl,
 }: SocialPropTypes) {
   return (
-    <Card className={`group ${widgetBgColor}`}>
-      <div className="flex h-full w-full items-center justify-center">
-        <a href={socialUrl} target="_blank" rel="noreferrer">
-          {children}
-        </a>
-        <CardLinkButton
-          href={socialUrl}
-          targetBlank
-          bgColorClass={linkBgColor}
-          hoverRingClass={linkRingColor}
-        />
-      </div>
+    <Card
+      className={twMerge(
+        "group flex h-full w-full items-center justify-center",
+        widgetBgColor,
+      )}
+    >
+      <a href={socialUrl} target="_blank" rel="noreferrer">
+        {children}
+      </a>
+      <CardLinkButton
+        href={socialUrl}
+        targetBlank
+        bgColorClass={linkBgColor}
+        hoverRingClass={linkRingColor}
+      />
     </Card>
   );
 }
