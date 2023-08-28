3a277b5d63395dc9f4cd5a938a860bc2ce0306c3
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2023-08-29 01:51:04 +0530

    ProjectsWidget: Remove one level of nesting

--- app/components/widgets/ProjectsWidget.tsx
+++ app/components/widgets/ProjectsWidget.tsx
@@ -5,11 +5,9 @@ import { ArrowRightIcon } from "@heroicons/react/24/outline";
 
 export default function ProjectsWidget() {
   return (
-    <Card>
-      <div className="flex h-full w-full select-none flex-col items-center justify-center">
-        <span className="text-2xl font-bold">Projects and works</span>
-        <CardLinkButton href="/projects" variant={ArrowRightIcon} />
-      </div>
+    <Card className="flex h-full w-full select-none flex-col items-center justify-center">
+      <span className="text-2xl font-bold">Projects and works</span>
+      <CardLinkButton href="/projects" variant={ArrowRightIcon} />
     </Card>
   );
 }
