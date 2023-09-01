4c79cdb068d22f846b2cce6c129d347484814920
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2023-09-02 01:30:46 +0530

    SpotifyWidget: Merge both spotify APIs into one

--- app/components/widgets/SpotifyWidget.tsx
+++ app/components/widgets/SpotifyWidget.tsx
@@ -1,67 +1,57 @@
 "use client";
 import useSWR from "swr";
 
-import { NowPlayingSong } from "@api/now-playing";
-import { RecentlyPlayedSong } from "@api/recently-played";
+import { SpotifyResponse } from "@api/spotify";
 import Card from "@components/Card";
 import MusicBars from "@components/MusicBars";
 import SpotifyLogo from "@drawables/spotify";
 import fetcher from "@lib/fetcher";
 
 export default function SpotifyWidget() {
-  const { data: nowPlaying, isLoading: nowPlayingLoading } =
-    useSWR<NowPlayingSong>("/api/now-playing", fetcher, {
-      revalidateIfStale: false,
+  const { data: songData, isLoading } = useSWR<SpotifyResponse>(
+    "/api/spotify",
+    fetcher,
+    {
       revalidateOnFocus: false,
       revalidateOnReconnect: false,
-    });
-  const { data: recentlyPlayed, isLoading: recentlyPlayedLoading } = useSWR<
-    RecentlyPlayedSong[]
-  >("/api/recently-played", fetcher, {
-    revalidateIfStale: false,
-    revalidateOnFocus: false,
-    revalidateOnReconnect: false,
-  });
-
-  const loading = nowPlayingLoading || recentlyPlayedLoading;
-
-  const recentSong = recentlyPlayed?.[0];
+    },
+  );
 
   return (
     <Card className="group flex h-full flex-col justify-between p-8">
       <SpotifyLogo width={72} height={72} className="fill-spotify" />
       <div>
         <div>
-          {nowPlaying?.isPlaying ? (
+          {songData?.isPlaying ? (
             <div className="flex items-center">
               <MusicBars />
               <span className="ml-2 select-none font-bold text-spotify">
-                {loading ? "Looking up…" : "Listening"}
+                {isLoading ? "Looking up…" : "Listening"}
               </span>
             </div>
           ) : (
             <div className="flex items-center">
               <MusicBars />
               <div className="ml-2 select-none font-bold text-spotify">
-                {loading ? "Looking up…" : "Listened"}
+                {isLoading ? "Looking up…" : "Last played"}
               </div>
             </div>
           )}
           <div className="flex flex-col">
             <a
               className="max-w-max truncate text-xl font-extrabold text-neutral-700 hover:text-neutral-500"
-              href={nowPlaying?.songUrl || recentSong?.songUrl || "#"}
-              title={nowPlaying?.title || recentSong?.title || "No Song"}
+              href={songData?.songUrl || "#"}
+              title={songData?.title || "No Song"}
               target="_blank"
               rel="noopener noreferrer"
             >
-              {nowPlaying?.title || recentSong?.title || "No Song"}
+              {songData?.title || "No Song"}
             </a>
             <p
-              title={nowPlaying?.artist || recentSong?.artist || "No Artist"}
+              title={songData?.artist || "No Artist"}
               className="max-w-max cursor-default truncate text-sm text-neutral-700"
             >
-              {nowPlaying?.artist || recentSong?.artist || "No Artist"}
+              {songData?.artist || "No Artist"}
             </p>
           </div>
         </div>
