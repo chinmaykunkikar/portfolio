8601e01e3cb689a8aab923c9cb62361b3bb49035
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2023-09-02 00:45:09 +0530

    Navbar: Add some framer magic to the tabs It was between this one or the one like vercel site.

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,18 +1,11 @@
 "use client";
 
-import Button from "@components/Button";
 import fetcher from "@lib/fetcher";
+import { motion } from "framer-motion";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import useSWR from "swr";
 import { twMerge } from "tailwind-merge";
-
-type LinkWrapperProps = {
-  children: string | JSX.Element | JSX.Element[];
-  linkHref: string;
-  otherClasses?: string;
-  [props: string]: any;
-};
 
 type commitSHA = {
   sha: string;
@@ -30,29 +23,19 @@ export default function Navbar() {
       revalidateOnReconnect: false,
     },
   );
-
   const lastCommitSHA = data?.sha;
 
-  function LinkWrapper({
-    children,
-    linkHref,
-    otherClasses,
-    ...props
-  }: LinkWrapperProps) {
-    const activeLink = pathname === linkHref;
-    return (
-      <Link href={linkHref} className={otherClasses} passHref>
-        <span className={activeLink ? "text-neutral-500" : ""} {...props}>
-          {children}
-        </span>
-      </Link>
-    );
-  }
+  const tabs = [
+    { id: "home", label: "Home", link: "/" },
+    { id: "about", label: "About", link: "/about" },
+    { id: "projects", label: "Projects", link: "/projects" },
+    { id: "contact", label: "Contact", link: "/contact" },
+  ];
 
   return (
     <>
       <nav className="flex flex-col items-center justify-between gap-4 px-12 py-8 text-neutral-700 md:px-20 lg:flex-row lg:gap-0 lg:px-32">
-        <span className="md:text-xl">
+        <span className="md:text-lg">
           <h3 className="cursor-default	select-none	font-mono">
             [chinmay@web{" "}
             <a
@@ -71,18 +54,35 @@ export default function Navbar() {
           </h3>
         </span>
         <div className="flex h-fit items-center gap-1 md:gap-8">
-          <LinkWrapper linkHref="/">
-            <Button>Home</Button>
-          </LinkWrapper>
-          <LinkWrapper linkHref="/about">
-            <Button>About</Button>
-          </LinkWrapper>
-          <LinkWrapper linkHref="/projects">
-            <Button>Projects</Button>
-          </LinkWrapper>
-          <LinkWrapper linkHref="/contact">
-            <Button>Contact</Button>
-          </LinkWrapper>
+          <div className="flex space-x-1">
+            {tabs.map((tab) => (
+              <Link
+                key={tab.id}
+                href={tab.link}
+                passHref
+                className={twMerge(
+                  pathname === tab.link ? "" : "hover:text-neutral-800/70",
+                  "relative rounded-xl px-4 py-2 text-sm text-neutral-500 transition focus-visible:outline-2",
+                )}
+                style={{
+                  WebkitTapHighlightColor: "transparent",
+                }}
+              >
+                {pathname === tab.link && (
+                  <motion.span
+                    layoutId="bubble"
+                    className="absolute inset-0 z-10 rounded-xl bg-neutral-900 mix-blend-difference"
+                    transition={{
+                      type: "spring",
+                      bounce: 0.2,
+                      duration: 0.4,
+                    }}
+                  />
+                )}
+                {tab.label}
+              </Link>
+            ))}
+          </div>
         </div>
       </nav>
     </>
