805bebea86d344431fd779f33711cad8d31926c6
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2023-09-01 18:40:31 +0530

    SpotifyWidget: Avoid initial layout shifts

--- app/components/widgets/SpotifyWidget.tsx
+++ app/components/widgets/SpotifyWidget.tsx
@@ -10,10 +10,18 @@ import fetcher from "@lib/fetcher";
 
 export default function SpotifyWidget() {
   const { data: nowPlaying, isLoading: nowPlayingLoading } =
-    useSWR<NowPlayingSong>("/api/now-playing", fetcher);
+    useSWR<NowPlayingSong>("/api/now-playing", fetcher, {
+      revalidateIfStale: false,
+      revalidateOnFocus: false,
+      revalidateOnReconnect: false,
+    });
   const { data: recentlyPlayed, isLoading: recentlyPlayedLoading } = useSWR<
     RecentlyPlayedSong[]
-  >("/api/recently-played", fetcher);
+  >("/api/recently-played", fetcher, {
+    revalidateIfStale: false,
+    revalidateOnFocus: false,
+    revalidateOnReconnect: false,
+  });
 
   const loading = nowPlayingLoading || recentlyPlayedLoading;
 
@@ -23,49 +31,40 @@ export default function SpotifyWidget() {
     <Card className="group flex h-full flex-col justify-between p-8">
       <SpotifyLogo width={72} height={72} className="fill-spotify" />
       <div>
-        {loading ? (
-          <div className="flex items-center">
-            <AnimatedBars shouldAnimate />
-            <span className="ml-2 select-none font-bold text-spotify">
-              Loading...
-            </span>
-          </div>
-        ) : (
-          <div>
-            {nowPlaying?.isPlaying ? (
-              <div className="flex items-center">
-                <AnimatedBars shouldAnimate />
-                <span className="ml-2 select-none font-bold text-spotify">
-                  Listening
-                </span>
-              </div>
-            ) : (
-              <div className="flex items-center">
-                <AnimatedBars shouldAnimate={false} />
-                <div className="ml-2 select-none font-bold text-spotify">
-                  Listened
-                </div>
-              </div>
-            )}
-            <div className="flex flex-col">
-              <a
-                className="max-w-max truncate text-xl font-extrabold text-neutral-700 hover:text-neutral-500"
-                href={nowPlaying?.songUrl || recentSong?.songUrl || "#"}
-                title={nowPlaying?.title || recentSong?.title || "No Song"}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                {nowPlaying?.title || recentSong?.title || "No Song"}
-              </a>
-              <p
-                title={nowPlaying?.artist || recentSong?.artist || "No Artist"}
-                className="max-w-max cursor-default truncate text-sm text-neutral-700"
-              >
-                {nowPlaying?.artist || recentSong?.artist || "No Artist"}
-              </p>
+        <div>
+          {nowPlaying?.isPlaying ? (
+            <div className="flex items-center">
+              <AnimatedBars shouldAnimate />
+              <span className="ml-2 select-none font-bold text-spotify">
+                {loading ? "Looking up…" : "Listening"}
+              </span>
             </div>
+          ) : (
+            <div className="flex items-center">
+              <AnimatedBars shouldAnimate={false} />
+              <div className="ml-2 select-none font-bold text-spotify">
+                {loading ? "Looking up…" : "Listened"}
+              </div>
+            </div>
+          )}
+          <div className="flex flex-col">
+            <a
+              className="max-w-max truncate text-xl font-extrabold text-neutral-700 hover:text-neutral-500"
+              href={nowPlaying?.songUrl || recentSong?.songUrl || "#"}
+              title={nowPlaying?.title || recentSong?.title || "No Song"}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {nowPlaying?.title || recentSong?.title || "No Song"}
+            </a>
+            <p
+              title={nowPlaying?.artist || recentSong?.artist || "No Artist"}
+              className="max-w-max cursor-default truncate text-sm text-neutral-700"
+            >
+              {nowPlaying?.artist || recentSong?.artist || "No Artist"}
+            </p>
           </div>
-        )}
+        </div>
       </div>
     </Card>
   );
