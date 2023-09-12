1793e16667c57ce5f52f92dceec972eb67a60bf9
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2023-09-13 01:24:04 +0530

    Footer: Add coffee emoji and change name's font

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,19 +2,33 @@ import Image from "next/image";
 
 export default function Footer() {
   return (
-    <footer className="my-8 flex justify-center px-4 text-xs lg:px-16 lg:text-lg">
+    <footer className="mx-2 my-8 flex justify-center text-xs lg:px-16 lg:text-lg">
       <span className="text-neutral-500">
-        <span>Built with&nbsp;</span>
+        <span>Made with&nbsp;</span>
         <Image
           src="/emoji/heart.png"
           alt="love"
-          width={24}
-          height={24}
+          width={20}
+          height={20}
           className="inline"
           quality={100}
+          priority
+        />
+        <span>&nbsp;and&nbsp;</span>
+        <Image
+          src="/emoji/coffee.png"
+          alt="love"
+          width={28}
+          height={28}
+          className="inline"
+          quality={100}
+          priority
         />
         <span>
-          &nbsp;by <span className="font-bold">Chinmay Kunkikar</span>
+          &nbsp;by&nbsp;
+          <span className="font-newsreader lg:text-xl">
+            Chinmay&nbsp;Kunkikar
+          </span>
         </span>
       </span>
     </footer>
