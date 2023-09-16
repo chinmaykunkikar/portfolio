f18681e6cfd933a152167c2fa4629d5937bade3d
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2023-09-17 02:47:41 +0530

    About: Change some intro text to make it less repetitive

--- src/widgets/HiWidget.tsx
+++ src/widgets/HiWidget.tsx
@@ -3,7 +3,7 @@ import Card from "src/components/Card";
 export default function HiWidget() {
   return (
     <Card className="flex flex-col items-center justify-center">
-      <div className="text-7xl">Hi</div>
+      <div className="font-newsreader text-2xl">Chinmay Kunkikar</div>
     </Card>
   );
 }
--- src/widgets/IntroWidget.tsx
+++ src/widgets/IntroWidget.tsx
@@ -3,18 +3,14 @@ import Card from "src/components/Card";
 
 export default function IntroWidget() {
   return (
-    <Card className="flex w-full flex-col items-center justify-center p-8 text-sm text-neutral-500 md:col-span-2 md:p-4 lg:text-lg">
+    <Card className="text-type flex w-full flex-col items-center justify-center p-8 text-sm md:col-span-2 md:p-4 lg:text-lg">
       <Balancer>
         <p className="mb-2">
-          I&apos;m{" "}
-          <span className="font-extrabold lg:text-xl">Chinmay Kunkikar</span>, a
-          frontend developer from India, who is passionate about building
-          delightful web experiences. I&apos;m interested in React, Cloud
-          computing, Linux, Design, and Music.
+          I&apos;m passionate about building delightful web experiences.
         </p>
         <p className="mb-2">
-          I started my journey in web development during 2021 while I was
-          building my academic project, Vide.
+          I&apos;m interested in React, Cloud computing, Linux, Design, and
+          Music.
         </p>
         {/* <p className="mb-2">
           You might find me enjoying hot sips of coffee while musing over my
