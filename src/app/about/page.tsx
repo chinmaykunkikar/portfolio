439523ea956027606b559e94816597a886892fa5
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2023-09-16 17:18:04 +0530

    App: Increase grid density to 2x https://media.tenor.com/ZZ7lLVO1zZMAAAAC/surprise-tool-mickey-mouse.gif

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -8,13 +8,25 @@ import LinkedInWidget from "@widgets/social/LinkedInWidget";
 
 export default function About() {
   return (
-    <PageWrapper className="[grid-auto-columns: 18rem] [grid-auto-rows: 18rem] lg:grid-rows-[18rem 18rem 18rem] text-type mx-auto grid w-full max-w-[320px] grid-cols-1 gap-4 sm:max-w-screen-sm sm:grid-cols-2 lg:max-w-[1200px] lg:grid-cols-4">
-      <HiWidget />
-      <IntroWidget />
-      <PhotoWidget />
-      <ExperienceWidget />
-      <LinkedInWidget />
-      <ResumeWidget />
+    <PageWrapper className="[grid-auto-columns: 9rem] [grid-auto-rows: 9rem] lg:grid-rows-[9rem 9rem 9rem 9rem 9rem 9rem] mx-auto grid w-full max-w-[320px] grid-cols-2 gap-4 sm:max-w-screen-sm sm:grid-cols-4 lg:max-w-[1200px] lg:grid-cols-8">
+      <div className="col-span-2 row-span-2">
+        <HiWidget />
+      </div>
+      <div className="col-span-2 row-span-2 sm:col-span-4">
+        <IntroWidget />
+      </div>
+      <div className="col-span-2 row-span-2">
+        <PhotoWidget />
+      </div>
+      <div className="col-span-2 row-span-2 sm:col-span-4">
+        <ExperienceWidget />
+      </div>
+      <div className="col-span-2 row-span-2">
+        <LinkedInWidget />
+      </div>
+      <div className="col-span-2 row-span-2">
+        <ResumeWidget />
+      </div>
     </PageWrapper>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,32 +11,32 @@ import TwitterWidget from "@widgets/social/TwitterWidget";
 
 export default function Home() {
   return (
-    <PageWrapper className="[grid-auto-columns: 18rem] [grid-auto-rows: 18rem] lg:grid-rows-[18rem 18rem 18rem] mx-auto grid w-full max-w-[320px] grid-cols-1 gap-4 sm:max-w-screen-sm sm:grid-cols-2 lg:max-w-[1200px] lg:grid-cols-4">
-      <div className="order-1 sm:col-span-2">
+    <PageWrapper className="[grid-auto-columns: 9rem] [grid-auto-rows: 9rem] lg:grid-rows-[9rem 9rem 9rem 9rem 9rem 9rem] mx-auto grid w-full max-w-[320px] grid-cols-2 gap-4 sm:max-w-screen-sm sm:grid-cols-4 lg:max-w-[1200px] lg:grid-cols-8">
+      <div className="order-1 col-span-2 row-span-2 sm:col-span-4">
         <HelloWidget />
       </div>
-      <div className="order-2">
+      <div className="order-2 col-span-2 row-span-2">
         <GitHubWidget />
       </div>
-      <div className="order-3">
+      <div className="order-3 col-span-2 row-span-2">
         <MapWidget />
       </div>
-      <div className="order-4">
+      <div className="order-4 col-span-2 row-span-2">
         <ProjectsWidget />
       </div>
-      <div className="order-5">
+      <div className="order-5 col-span-2 row-span-2">
         <LinkedInWidget />
       </div>
-      <div className="order-6 sm:col-span-2">
+      <div className="order-6 col-span-2 row-span-2 sm:col-span-4">
         <SkillsWidget />
       </div>
-      <div className="order-7">
+      <div className="order-7 col-span-2 row-span-2">
         <SpotifyWidget />
       </div>
-      <div className="order-8">
+      <div className="order-8 col-span-2 row-span-2">
         <TwitterWidget />
       </div>
-      <div className="order-9 sm:col-span-2">
+      <div className="order-9 col-span-2 row-span-2 sm:col-span-4">
         <ContactWidget />
       </div>
     </PageWrapper>
