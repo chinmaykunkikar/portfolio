f2f9c5b4e92bd4748729905f3f72754ab1a9ad7a
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2023-09-22 01:24:16 +0530

    ExperienceWidget: Small changes

--- src/components/widgets/ExperienceWidget.tsx
+++ src/components/widgets/ExperienceWidget.tsx
@@ -3,14 +3,14 @@ import Card from "@components/Card";
 export default function ExperienceWidget() {
   return (
     <Card className="md:col-span-2">
-      <div className="w-full-h-full mt-16 flex flex-col justify-center px-4">
-        <div className="select-none text-3xl font-bold">Experience</div>
-        <div className="text-type mt-8 flex cursor-default flex-col font-mono">
-          <div className="my-2 flex items-center justify-between ">
+      <div className="w-full-h-full flex flex-col justify-center px-4 pt-4">
+        <div className="select-none font-newsreader text-4xl">Career</div>
+        <div className="mt-4 flex cursor-default flex-col text-type">
+          <div className="flex items-center justify-between">
             <div className="flex-1 text-sm font-bold md:text-xl md:font-normal">
               MetaBrainz Foundation
             </div>
-            <div className="mr-2 text-sm">Student Intern</div>
+            <div className="mr-2 text-sm">Web Developer</div>
             <div className="text-sm font-bold">2022</div>
           </div>
           <div className="my-2 flex items-center justify-between text-xl">
