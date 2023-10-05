86db7b5ba27c4c07675a87c2fbc73ba6aa9663a2
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2023-10-06 04:01:26 +0530

    Contributions: Make it a proper github card

--- src/components/Contributions.tsx
+++ src/components/Contributions.tsx
@@ -2,7 +2,9 @@
 
 import Calendar from "@components/Calendar";
 import Card from "@components/Card";
+import GitHubLogo from "@drawables/github";
 import fetcher from "@lib/fetcher";
+import Link from "next/link";
 import useSWRImmutable from "swr";
 
 export default function Contributions() {
@@ -10,5 +12,25 @@ export default function Contributions() {
 
   const contributionCalendar =
     data?.contributionsCollection?.contributionCalendar;
-  return <Card>{data && <Calendar data={contributionCalendar} />}</Card>;
+  return (
+    <Card className="flex flex-col justify-between">
+      <div className="flex items-center justify-between">
+        <GitHubLogo width={72} height={72} className=" fill-brand-github" />
+        <Link
+          passHref
+          href="https://github.com/chinmaykunkikar"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <button className="rounded-full bg-brand-github px-4 py-2 text-sm text-neutral-100 transition-colors hover:bg-neutral-700">
+            Follow
+          </button>
+        </Link>
+      </div>
+      <div>
+        <p className="ml-1 select-none font-bold">Chinmay Kunkikar</p>
+      </div>
+      {data && <Calendar data={contributionCalendar} />}
+    </Card>
+  );
 }
